7dc8bf44c122ccf2bfe5e62f0b849fa6db2b11e1
Hallgrenaren <38402142+Hallgrenaren@users.noreply.github.com>
2024-11-10 14:53:55 +0100

    Navbar and a little skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 //import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Body from './components/Body.jsx'
 
 function App() {
-  //const [count, setCount] = useState(0)
+    //const [count, setCount] = useState(0)
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <div className="container">
+            <Navbar />
+            <Body />
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <h3 className="welcome_text">My portfolio site!</h3>
+            <h4 className="nav_name">Martin Hallgren</h4>
+        </nav>
+    )
+}
